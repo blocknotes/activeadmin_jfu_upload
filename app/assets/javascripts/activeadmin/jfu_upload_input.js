b81e9aaa110c00075352fd5dd05754815677d320
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2018-04-05 15:05:17 +0200

    Improved upload_file method

--- app/assets/javascripts/activeadmin/jfu_upload_input.js
+++ app/assets/javascripts/activeadmin/jfu_upload_input.js
@@ -2,10 +2,23 @@ $(document).ready( function() {
   $('.jfu_upload_input').each(function () {
     var options = {
       dataType: 'json',
+      add: function (e, data) {
+        // data.context = $('<p/>').text('Uploading...').appendTo(document.body);
+        // data.submit();
+        $(e.target).next().html('<li class="jfu_loading">Uploading...</li>');
+      },
       done: function (e, data) {
         if( data.result.result == 1 ) {
           $(e.target).next().html('<li class="jfu_file">' + data.result.file_name + '</li>');
         }
+      },
+      progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        console.log(progress);
+        // $('#progress .bar').css(
+        //   'width',
+        //   progress + '%'
+        // );
       }
     };
     options = $.extend({}, options, $(this).data('options'));
