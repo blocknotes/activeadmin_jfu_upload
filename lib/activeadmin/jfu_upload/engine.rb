7d2148a540d899bf8221077bb82ed381a031ed33
Mattia Roccoberton <mattia.roccoberton@develondigital.com>
2018-04-05 11:13:04 +0200

    Improved upload_file method

--- lib/activeadmin/jfu_upload/engine.rb
+++ lib/activeadmin/jfu_upload/engine.rb
@@ -17,12 +17,23 @@ module ActiveAdmin
               if m
                 pos = m[1].to_i
                 if pos == 0
-                  resource.update field => param
-                else
-                  File.open( resource.try( field ).try( :path ), 'ab' ) { |f| f.write( param.read ) }
+                  # resource.update field => param
+                  dir = Rails.root.join('tmp', 'uploads')
+                  dir.mkdir unless File.exists?(dir)
+                  @@last_filename = param.original_filename  # TODO: fix me!
+                  resource.update_column field, dir.join(param.tempfile).to_s  # alternative: "#{resource.class.to_s.tableize}_#{resource.id}_#{Time.now.to_i}"
+                  # else
+                  #   File.open( resource.try( field ).try( :path ), 'ab' ) { |f| f.write( param.read ) }
                 end
+                File.open(resource.read_attribute(field), 'ab') { |f| f.write(param.read) }
                 if ( m[2].to_i + 1 ) == m[3].to_i
-                  response = { result: 1, pos: m[3].to_i, file_name: resource.try( field ).try( :url ) }
+                  file = resource.read_attribute(field)
+                  path = Pathname.new file
+                  dst = path.dirname.join(@@last_filename).to_s
+                  File.rename(file, dst)
+                  resource.send("#{field}=", File.open(dst))
+                  resource.save
+                  response = { result: 1, pos: m[3].to_i, file_name: resource.try(field).try(:url) }
                 else
                   response = { result: 2, position: pos }
                 end
